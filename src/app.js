c34fab1dad9f086b88c3f40ce274655ee654dd02
g0kuluppal08 <97520505+g0kuluppal08@users.noreply.github.com>
2024-01-08 06:16:55 +0530

    all setup commit

--- src/app.js
+++ src/app.js
@@ -14,4 +14,24 @@ app.use(express.urlencoded({extended:true,limit:"16kb"}));
 app.use(express.static("public"));
 app.use(cookieParser()); //its main work is to perform crud operations on user browser using server
 
+
+//routes import
+import userRouter from "./routes/user.routes.js";
+
+
+//routes declaration
+app.use("/api/v1/users",userRouter);
+
+//http://localhost:8000/api/v1/users/register
+
+
+
+
+
+
+
+
+
+
+
 export {app};
